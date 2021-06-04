b3be10a4f8e6829ae71e0baf93a6961fab01bc3b
William Lengenfelder <willfelder@users.noreply.github.com>
2021-06-04 13:46:53 -0300

    Updating the homepage
    Created the buttons and all layout of the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import Item from './components/Item/index'
 import './App.css';
+
+import Acessories from './assets/Desktop-Accessories.jpg'
+import ModelS from './assets/Desktop-ModelS.jpeg'
+import Model3 from './assets/Desktop-Model3.jpeg'
+import ModelX from './assets/Desktop-ModelX.jpeg'
+import ModelY from './assets/Desktop-ModelY.jpeg'
+import SolarPanels from './assets/Desktop-SolarPanels.jpeg'
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Header />
+
+       <div className="app__itemsContainer">
+         <Item
+              title="Lowest Cost Solar Panels in America"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={SolarPanels}
+              leftBtnTxt="ORDER NOW"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true"
+              first />
+
+         <Item
+              title="Model S"
+              desc="$69,420"
+              descLink=""
+              backgroundImg={ModelS}
+              leftBtnTxt="CUSTOM ORDER"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+         <Item
+              title="Model 3"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={Model3}
+              leftBtnTxt="CUSTOM ORDER"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+         <Item
+              title="Model X"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={ModelX}
+              leftBtnTxt="CUSTOM ORDER"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+         <Item
+              title="Model Y"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={ModelY}
+              leftBtnTxt="CUSTOM ORDER"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+         <Item
+              title="Solar for New Roofs"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={SolarRoof}
+              leftBtnTxt="ORDER NOW"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+         <Item
+              title="Acessories"
+              desc="Money-back guarantee"
+              descLink=""
+              backgroundImg={Acessories}
+              leftBtnTxt="SHOP NOW"
+              leftBtnLink=""
+              rightBtnTxt="LEARN MORE"
+              rightBtnLink=""
+              twoButtons="true" />
+
+       </div>
+
     </div>
   );
 }
