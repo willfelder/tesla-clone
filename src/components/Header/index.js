fb5d99c83df1b5808856989c9a20e09ab4fed28f
William Lengenfelder <willfelder@users.noreply.github.com>
2021-06-18 12:25:54 -0300

    deploy app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Header.css'
 import TeslaLogo from '../../assets/teslaLogoSmall.svg'
-import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
 
@@ -13,22 +12,19 @@ const Header = () => {
                  <img src={TeslaLogo} alt="Tesla Logo" />
              </div>
 
-             <div className="header__center">
+             <nav className="header__center">
                  <p>Model S</p>
                  <p>Model 3</p>
                  <p>Model X</p>
                  <p>Model Y</p>
                  <p>Solar Roof</p>
                  <p>Solar Panel</p>
-             </div>
+             </nav>
 
-             <div className="header__right">
+             <nav className="header__right">
                  <p>Shop</p>
                  <p>Tesla Account</p>
-                     <div className="responsive__menu">
-                        <MenuIcon /> 
-                     </div>
-             </div>
+             </nav>
 
         </div>
     )
